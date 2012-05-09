5f344cc192a88d64502441ed9a8644aa8028897f
Yury Brodskiy <y.brodskiy@utwente.nl>
2012-05-09 16:46:59 +0200

    Fixed deployment script + added gitsubmodule fbsched.

--- YouBot_deployment/rtt_deploy/run.sh
+++ YouBot_deployment/rtt_deploy/run.sh
@@ -17,10 +17,11 @@ echo $LUA_PATH
 
 export RTT_MOTION_CONTROL_MSGS_PATH=$(rospack find rtt_motion_control_msgs)/lib
 export YOUBOT_ADAPTERS=$(rospack find YouBot_adapters)/lib/orocos
+export TSIM_ADAPTERS=$(rospack find TSimAdapters)/lib/orocos
 export YOUBOT_OODL_PATH=$(rospack find YouBot_OODL)/lib/orocos
 export RTT_PATH=$(rospack find rtt)/../install
 export OCL_PATH=$(rospack find ocl)/../install/lib/orocos/gnulinux
 
-export RTT_COMPONENT_PATH=${RTT_PATH}:${OCL_PATH}:${RTT_MOTION_CONTROL_MSGS_PATH}:${YOUBOT_OODL_PATH}:${YOUBOT_ADAPTERS}
+export RTT_COMPONENT_PATH=${RTT_PATH}:${OCL_PATH}:${RTT_MOTION_CONTROL_MSGS_PATH}:${YOUBOT_OODL_PATH}:${YOUBOT_ADAPTERS}:${TSIM_ADAPTERS}
 
 rosrun ocl deployer-gnulinux -s $1 $2 $3 $4 $5
