2b0f7d3ea32014524ad7374c3de34ee7ce12dbb7
youbot <youbot@wallace.(none)>
2012-10-16 17:27:48 +0200

    [BRSU] Update of scripts.

--- YouBot_deployment/rtt_deploy/debug.sh
+++ YouBot_deployment/rtt_deploy/debug.sh
@@ -3,8 +3,10 @@
 source /opt/ros/electric/setup.bash;
 source /opt/ros/electric/stacks/orocos_toolchain/env.sh;
 
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/DEV
-
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/youbot/ros_stacks
+export ROS_MASTER_URI=http://wallace:11311
+#export ROS_HOSTNAME=10.0.3.190
+source ./start_roscore.sh
 export YOUBOT_PATH=$(rosstack find youbot-stack)
 
 RTTLUA_MODULES=`rospack find ocl`/lua/modules/?.lua
@@ -20,8 +22,12 @@ export YOUBOT_ADAPTERS=$(rospack find YouBot_adapters)/lib/orocos
 export YOUBOT_OODL_PATH=$(rospack find YouBot_OODL)/lib/orocos
 export RTT_PATH=$(rospack find rtt)/../install
 export OCL_PATH=$(rospack find ocl)/../install
+export JOYSTICK_PATH=$(rospack find YouBot_joystick)/lib/orocos
 
-export RTT_COMPONENT_PATH=${RTT_PATH}:${OCL_PATH}/lib/orocos/gnulinux:${RTT_MOTION_CONTROL_MSGS_PATH}:${YOUBOT_OODL_PATH}:${YOUBOT_ADAPTERS}
+export RTT_COMPONENT_PATH=${RTT_PATH}:${OCL_PATH}:${RTT_MOTION_CONTROL_MSGS_PATH}:${YOUBOT_OODL_PATH}:${YOUBOT_ADAPTERS}:${JOYSTICK_PATH}
+
+export ORO_LOGLEVEL=6
+export BASE_LOG_LEVEL="DEBUG"
 
 gdb --args ${OCL_PATH}/bin/deployer-gnulinux -ldebug -s $1 -- $2 $3 $4 $5
 
--- YouBot_deployment/rtt_deploy/run.sh
+++ YouBot_deployment/rtt_deploy/run.sh
@@ -3,7 +3,9 @@
 source /opt/ros/electric/setup.bash;
 source /opt/ros/electric/stacks/orocos_toolchain/env.sh;
 
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/DEV
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/ros_stacks
+
+export ROS_MASTER_URI=http://robert:11311
 
 export YOUBOT_PATH=$(rosstack find youbot-stack)
 
